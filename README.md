34c4a0379cc28008e2bf21bed7a7b989bdb0efe7
John Ta <john.ta@student.csulb.edu>
2019-12-26 20:26:10 -0800

    Overhauled Program
    Now works by downloading first, then playing locally instead of streaming from YouTube

--- README.md
+++ README.md
@@ -10,31 +10,6 @@ ___
 
 `npm install discord.js --save`
 
-### ytdl and opusscript
+### opusscript
 
-`npm install --save ytdl-core opusscript`
-
-### mysql
-
-`npm install --save mysql`
-
-___
-
-## Using MySql
-
-Reference: https://www.youtube.com/watch?v=E5TVwyiCuYA&list=LLzew89ukV1_bX5G_-D8-16A&index=3&t=0s
-
-### Useful MySQL commands:
-
-`SHOW DATABASES;`: to see all of your databases
-`USE sbdb;`: to select a database
-`CREATE TABLE <tableName> (columnName1, column2, ...);`: to create tables in your database
-`SHOW TABLES`: view tables in database
-`DESCRIBE <tableName>`: view details about a table
-
-### Data types to use when creating columns
-
-**`<columnName> <dataType> <nullType>`**: general column syntax
-`<columnName> VARCHAR(<size>) NOT NULL`: creates column columnName that's a varchar of size that can't be empty when the row is created.
-`<columnName> INT`: creates column columnName that's an int that can be empty when the row is created.
-___
+`npm install --save opusscript`
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Load required libraries
 const Discord = require("discord.js");
-const mysql = require("mysql");
-const ytdl = require("ytdl-core");
+const exec = require("child_process").exec;
 
 // Load data to run bot and database
 const { token, 
@@ -12,21 +11,23 @@ const { token,
         database
 } = require("./config.json");
 
+
+
 // Start up discord client
 const client = new Discord.Client();
 
-// Connect to database
-var con = mysql.createConnection({host, user, password, database});
-con.connect(err => {
-    if (err) console.error(err);
-    console.log("Connected to database!");
-})
+// Set up connections for streaming YouTube
+const streamOptions = { seek: 0, volume: 1 };
+var queue = [];
 
 // Log into client
 client.login(token);
 
 // Function to pull ID of a YouTube video
 function youtubeParser(url){
+    if (url === undefined) {
+        return [false, null];
+    }
     var regExp = /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*)?(\?t=)?(\d*)/;
     var match = url.match(regExp);
     return [match&&match[1].length==11 ? match[1] : null, match&&match[3].length!=0 ? match[3] : null];
@@ -42,10 +43,38 @@ function durationValue(duration) {
     return match&&match[0].length!=0 ? match[0] : null;
 }
 
+// Function to grab section of YouTube MP3, uses command line code
+function mp3Cutter(ytid, id, startSeconds, durationSeconds) {
+    if (durationSeconds == 0) {
+        durationSeconds = 5;
+    }
+    
+    startFormat = new Date(startSeconds*1000).toISOString().substr(11, 8);
+    endFormat = new Date((startSeconds + durationSeconds)*1000).toISOString().substr(11, 8);
+
+    console.log("\tDownloading...");
+    const child1 = exec("youtube-dl -g " + ytid, function(err, result) {
+        if (err) return console.log(err);
+        console.log("\t\tGrabbed URL");
+        var url = result.split("\n")[1];
+        const child2 = exec("ffmpeg -i " + '"'+url+'"' + " -ss " + startFormat + " -to " + endFormat + " intro/" + id + ".mp3", function(err, result) {
+            if (err) return console.log(err);
+            console.log("\t\tDownload Complete");
+        });
+
+        child2.stdin.write("Y");
+        child2.stdin.end();
+    });
+}
+
+
+
+// Displays to console when Siggy is ready to go
 client.on("ready", () => {
     console.log("Music bot is ready!");
 });
 
+// Commands for when a message is sent
 client.on("message", (message) => {
     if (!message.content.startsWith(prefix) || message.author.bot) {
         // Returns if message starts with wrong prefix or is sent from a bot
@@ -61,29 +90,52 @@ client.on("message", (message) => {
         case 'intro':
             // Checks to see if second argument is a YouTube link
             [parsedYTID, parsedTime]  = youtubeParser(args[1]);
+            
             if (!parsedYTID) {
                 message.channel.send("You need to provide a YouTube link!");
                 return;
             }
             
-            // Update database
-            con.query(`SELECT * FROM intro where id = '${message.author.id}'`, (err, rows) => {
-                if (err) console.error(err);
-                
-                let sql; 
-                if (rows.length < 1) {
-                    sql = `INSERT INTO intro (id, yt, start, duration) VALUES('${message.author.id}', '${parsedYTID}', ${parsedTime}, ${durationValue(args[2])})`;
-                } else {
-                    let yt = rows[0].yt;
-                    sql = `UPDATE intro SET 
-                        yt = '${parsedYTID}', 
-                        start = ${parsedTime}, 
-                        duration = ${durationValue(args[2])} 
-                        WHERE id = '${message.author.id}'`;
-                }
-                con.query(sql, console.log);
-            });
-            // Display user's intro
+            duration = durationValue(args[2]);
+            mp3Cutter(parsedYTID, message.author.id, +parsedTime, +duration);
         break;
     }
 });
+
+// Commands for when a person joins, leaves, or moves channels.
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+    if (oldMember.user.bot || newMember.user.bot) {
+        return;
+    }
+
+    let newUserChannel = newMember.voiceChannel;
+    let oldUserChannel = oldMember.voiceChannel;
+
+    if (oldUserChannel === undefined && newUserChannel !== undefined) {
+        // User joins a channel
+        console.log("\t" + newMember.user.username + " joined " + newUserChannel.name);
+        play(newMember, "intro");
+    } else if (newUserChannel === undefined) {
+        // User leaves a channel
+        console.log("\t" + oldMember.user.username + " left " + oldUserChannel.name);
+        //play(newMember, "outro");
+    } else if (oldUserChannel && newUserChannel && oldUserChannel.id != newUserChannel.id) {
+        // User moved channels
+        console.log("\t" + newMember.user.username + " moved from " + oldUserChannel.name + " to " + newUserChannel.name);
+        play(newMember, "intro");
+    }
+});
+
+function play(newMember, table) {
+    let VoiceChannel = newMember.guild.channels.find(channel => channel.id === newMember.voiceChannel.id);
+    if (VoiceChannel != null) {
+        console.log("\t\t" + VoiceChannel.name + " was found and is a " + VoiceChannel.type + " channel.");
+        VoiceChannel.join()
+        .then(connection => {
+            console.log("\t\tBot joined the channel.");
+            const dispatcher = connection.playFile("intro/" + newMember.id + ".mp3");
+            //const dispatcher = connection.playFile("intro/" + newMember.id + ".mp3");
+        })
+        .catch(console.error)
+    }
+}
