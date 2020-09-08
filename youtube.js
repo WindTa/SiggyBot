7fcc37e7090154ab985f84530148a969b00a70f1
windta <john.k.tata@gmail.com>
2020-09-08 12:13:43 +0100

    completed changes to be more specific in start time and duration

--- youtube.js
+++ youtube.js
@@ -12,7 +12,8 @@ function urlParser(url){
     }
 
     // Returns two arguments: YouTube video ID, YouTube video timestamp (if provided)
-    var regExp = /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*)?(\?t=)?(\d*)/;
+    //var regExp = /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*)?(\?t=)?(\d*)/;
+    var regExp = /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*)?(\?t=)?(\d*(\.\d+)?)/;
     var match = url.match(regExp);
     return [match&&match[1].length==11 ? match[1] : null, match&&match[3].length!=0 ? match[3] : null];
 }
@@ -25,7 +26,8 @@ function durationValue(duration) {
     }
 
     // Returns duration
-    var regExp = /(\d*)/;
+    //var regExp = /(\d\.\d*)/;
+    var regExp = /\d*(\.\d+)?/;
     var match = duration.match(regExp);
     return match&&match[0].length!=0 ? match[0] : null;
 }
@@ -35,9 +37,9 @@ function mp3Cutter(ytid, id, startSeconds, durationSeconds, channel) {
     if (durationSeconds == 0) {
         durationSeconds = 5;
     }
-    
-    startFormat = new Date(startSeconds*1000).toISOString().substr(11, 8);
-    endFormat = new Date((startSeconds + durationSeconds)*1000).toISOString().substr(11, 8);
+
+    startFormat = new Date(startSeconds*1000).toISOString().substr(15, 8);
+    endFormat = new Date((startSeconds + durationSeconds)*1000).toISOString().substr(15, 8);
 
     channel.send(download).then((msg) => {
         const child1 = exec("youtube-dl -g -- " + ytid, function(err, result) {
@@ -46,7 +48,9 @@ function mp3Cutter(ytid, id, startSeconds, durationSeconds, channel) {
                 return console.log(err);
             }
             var url = result.split("\n")[1];
-            const child2 = exec("ffmpeg -i " + '"'+url+'"' + " -ss " + startFormat + " -to " + endFormat + " intro/" + id + ".mp3", function(err, result) {
+            var command = "ffmpeg -i " + '"'+url+'"' + " -ss " + startFormat + " -to " + endFormat + " intro/" + id + ".mp3";
+            console.log(command);
+            const child2 = exec(command, function(err, result) {
                 if (err) {
                     msg.edit(error);
                     return console.log(err);
